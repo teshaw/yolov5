8b9d0c7876606c33941a861bd232724ab6a4461c
teshaw <tomshaw.work@gmail.com>
2023-06-08 10:23:06 +0100

    Update detectors.py
    frame range support defaults in sorter

--- detectors.py
+++ detectors.py
@@ -19,11 +19,13 @@ from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device#, smart_inference_mode
 from utils.augmentations import letterbox
 
-def sorter(fp,default=1E9):
+def sorter(fp,default=1E9,start=0,end=1E9):
     _,fn = os.path.split(fp)
     if "_" in fn:
         try:
             result = int(fn.split("_",maxsplit=1)[0])
+            if result < start or result > end:
+                result=None
         except:
             result =  default
     else:
